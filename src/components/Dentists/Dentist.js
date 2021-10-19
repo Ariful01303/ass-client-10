382a218494c7465f9fe483f1d6fc157e66e0321c
Ariful01303 <arifhossen01303@gmail.com>
2021-10-19 15:53:08 +0600

    complite

--- src/components/Dentists/Dentist.js
+++ src/components/Dentists/Dentist.js
@@ -3,7 +3,7 @@ import './Dentist.css'
 const Dentist = (props) => {
    const {name,pic,dentics}=props.doctor
     return (
-       <div   className="col-lg-4 col-md-6 col-sm-12 col-12 p-3 doctors">
+       <div   className="col-lg-4 col-md-6 col-sm-12 col-12 p-3 doctors extra">
             <div>
             <img className="img-fluid image-size" src={pic} alt="" />
             <h3>Our dentist: {name}</h3>
--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -10,12 +10,14 @@ const Service = (props) => {
     }
     return (
         
-            <div className="col-lg-4 col-md-6 col-sm-12 col-12 extra-style p-5">
+            <div className="col-lg-4 col-md-6 col-sm-12 col-12 extra-style extra p-5">
             <img className="img-fluid" src={photo} alt="" />
-            <h4>{name}</h4>
+           <div className="p-2">
+           <h4>{name}</h4>
             <p>{title}</p>
             <button onClick={detailHandlar} className="btn btn-primary text-center ">Details</button>
           
+           </div>
         </div>
     );
 };
